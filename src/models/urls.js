0eed1ed422eb6ac330f4efcb87d74b75eb994357
vitorlc <vitor_cirqueira1@hotmail.com>
2020-07-28 21:29:06 -0300

    change structure

--- src/models/urls.js
+++ src/models/urls.js
@@ -1,9 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
-    const Urls = sequelize.define('Urls', {
+    const Urls = sequelize.define('urls', {
         original: DataTypes.STRING,
         shortUrl: DataTypes.STRING,
         clicks: DataTypes.INTEGER,
+    },{
+        tableName: 'urls'
     });
-
     return Urls;
 }
