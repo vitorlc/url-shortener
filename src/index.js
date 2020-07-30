2f6cab87f6eb2b38f578d300b120e800b6b258ac
vitorlc <vitor_cirqueira1@hotmail.com>
2020-07-30 08:23:18 -0300

    add morgan for better loging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const routes = require('./routes')
 
 const app = express()
 
+app.use(morgan('short'))
 app.use(express.json())
 app.use(routes)
 
