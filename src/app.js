29d713c699f8f3641a63fb959a02b9e11245df49
vitorlc <vitor_cirqueira1@hotmail.com>
2020-07-30 18:31:19 -0300

    separate listen port from aplication

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-  path: process.env.NODE_ENV == 'test' ? '.env.test' : '.env'
+    path: process.env.NODE_ENV == 'test' ? '.env.test' : '.env'
 })
 
 const express = require('express')
@@ -10,12 +10,10 @@ const routes = require('./routes')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/public'))
 app.use(morgan('short'))
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!')
-})
+module.exports = app
