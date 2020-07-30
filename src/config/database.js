e7fd77f967506abf1b960de6f7ce2f935924cceb
vitorlc <vitor_cirqueira1@hotmail.com>
2020-07-30 17:54:34 -0300

    configure environments

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,18 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV == 'test' ? '.env.test' : '.env'
+})
+
 module.exports = { 
   development: {
-    username: "root",
-    password: '12345678',
-    database: "database_development",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "mysql",
+    storage: './__tests__/database.sqlite'
+  },
+  test: {
+    dialect: process.env.DB_DIALECT,
+    storage: './__tests__/database.sqlite'
   }
 }
