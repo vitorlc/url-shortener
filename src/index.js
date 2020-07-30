e77a3c27cfc4f23964ae25957383e0fe3f8cdee7
vitorlc <vitor_cirqueira1@hotmail.com>
2020-07-30 09:22:32 -0300

    add static page for public test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const routes = require('./routes')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(__dirname + '/public'))
 app.use(morgan('short'))
 app.use(express.json())
 app.use(routes)
